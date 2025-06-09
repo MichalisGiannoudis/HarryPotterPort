b594ac65d4977248f11839d4806f891580145543
Michalis Giannoudis <michalis221@gmail.com>
2025-06-10 00:23:59 +0300

    changed spinner to be next to search bar while fetching or searching

--- front_end/src/components/spinnerComponent.tsx
+++ front_end/src/components/spinnerComponent.tsx
@@ -1,7 +1,7 @@
 const SpinnerComponent = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="w-20 h-20 border-4 border-gray-500 border-t-transparent rounded-full animate-spin"></div>
+    <div className="flex justify-center items-center mb-3">
+      <div className="w-5 h-5 border-4 border-gray-500 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 };
--- front_end/src/pages/index.tsx
+++ front_end/src/pages/index.tsx
@@ -9,20 +9,13 @@ export default function HomePage() {
   const [searchValue, setSearchValue] = useState('');
   const { houses, fetching, error } = useHouses(searchValue);
 
-  if (fetching) {
-    return <div>
-      <SpinnerComponent />
-    </div>;
-  }
-  
-  if (error) {
-    return <div className="text-red-500 flex justify-center items-center min-h-screen">Error: {error}</div>;
   }
 
   return (
-    <div className="container mx-auto p-4">
-      <SearchInput placeholder='Search houses' searchValue={searchValue} onSearchChange={setSearchValue}/>
-      <HouseList houses={houses} />
-    </div>
+    <div className="container mx-auto p-4 max-w-[800px]">
+      <div className='flex gap-4'>       
+        <SearchInput placeholder={houseSearchLabel} searchValue={searchValue} onSearchChange={handleSearch} />
+        {(fetching || searchInProgress) && <SpinnerComponent />}
+      </div>
   );
 }
