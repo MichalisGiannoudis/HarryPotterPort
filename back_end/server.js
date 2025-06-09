252ff887e50217c69b29310005f928ea9d2651b7
Michalis Giannoudis <michalis221@gmail.com>
2025-06-09 12:18:11 +0300

    call to fetch data on start up

--- back_end/server.js
+++ back_end/server.js
@@ -1,7 +1,9 @@
 const app = require('./src/app');
+const { fetchHousesData } = require('./src/controllers/fetchHousesController');
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () =>{
+  await fetchHousesData();
   console.log(`Server running on http://localhost:${PORT}`);
 });
