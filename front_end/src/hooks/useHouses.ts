1fb1f744bd5c93f83dcaa10e7e043e35b9bd038b
Michalis Giannoudis <michalis221@gmail.com>
2025-06-09 11:07:30 +0300

    Added the debounce hook and modified the hook that fetches the houses

--- front_end/src/hooks/useHouses.ts
+++ front_end/src/hooks/useHouses.ts
@@ -1,38 +1,51 @@
 import { useEffect, useState} from 'react';
 import { House } from '@/models/house.interface';
+import { useDebounce } from '@/hooks/useDebounce';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const useHouses = () => {
+export const useHouses = (searchValue: string = '') => {
     const [houses, setHouses] = useState<House[]>([]);
     const [fetching, setFetching] = useState(true);
     const [error, setError] = useState<string | null>(null);
 
+    // Use the custom hook to debounce the search value for 300 milliseconds
+    const debouncedSearchValue = useDebounce(searchValue, 300); 
+
     useEffect( () => {
     
         const fetchHouses = async () => {
-        try {
-            const res = await fetch(`${API_URL}/houses`);
-            if (!res.ok) {
-                throw new Error('Failed to fetch houses');
-            }
-            const data: House[] = await res.json();
-            setHouses(data);
-        } catch (err : unknown) {
-            if (err instanceof Error) {
-                setError(err.message);
-            } else {
-                setError('An unknown error occurred');
-            }
-            setHouses([]);
-        } finally {
-            setFetching(false);
-        }
-    };
 
-    fetchHouses();
+            setFetching(true);
+            setError(null);
 
-  }, []);
+            try {
+                const queryParam = debouncedSearchValue
+                    ? `?name=${debouncedSearchValue}`
+                    : '';
+                const res = await fetch(`${API_URL}/houses${queryParam}`);
+                
+                if (!res.ok) {
+                    throw new Error('Failed to fetch houses');
+                }
+                const data: House[] = await res.json();
+
+                setHouses(data);
+            } catch (err : unknown) {
+                if (err instanceof Error) {
+                    setError(err.message);
+                } else {
+                    setError('An unknown error occurred');
+                }
+                
+                setHouses([]);
+            } finally {
+                setFetching(false);
+            }
+        };
+
+        fetchHouses();
+    }, [debouncedSearchValue]);
 
     return { houses, fetching, error };
 };
